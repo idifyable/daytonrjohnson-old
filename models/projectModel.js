6584cacfc4ea510fe682f6cd2bc47a535512509d
idifyable <daytonrjohnson@gmail.com>
2018-12-16 18:44:08 -0600

    DRJ - Adding synchronization of the project model to  the database

--- models/projectModel.js
+++ models/projectModel.js
@@ -12,7 +12,7 @@ var Sequelize = require('sequelize');
 
 var Project = function(sequelize, Sequelize)
 {
-  const Project = sequelize.define('Project', {
+  const Project = sequelize.define('project', {
     title: Sequelize.TEXT,
     image: Sequelize.TEXT,
     technologies_used: Sequelize.ARRAY(Sequelize.TEXT),
@@ -20,11 +20,19 @@ var Project = function(sequelize, Sequelize)
     link_repo: Sequelize.TEXT
   },
   {
-    timestamps: false
+    timestamps: false,
+    freezeTableName: true
+  });
+
+  // Create the project table if one does not exist
+  Project.sync().then(function(response)
+  {
+    console.log(response);
   });
 
   return Project;
 }
+
 
 /****************************************
 * Exports
